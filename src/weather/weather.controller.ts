e08acda214a2ccbd3f542862546c118d02895c55
Bartłomiej B <bartlomiejb@MacBook-Pro-Bartomiej.local>
2024-01-07 16:14:15 +0100

    Apply swagger

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { ApiKeyGuard } from 'src/guards/ApiKeyGuard';
 import { Weather } from 'src/types/Weather';
@@ -11,7 +12,12 @@ export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get()
+  @ApiTags('weather')
   @UseGuards(ApiKeyGuard)
+  @ApiResponse({
+    status: 200,
+    type: Promise<Weather>,
+  })
   getWeatherByLocation(
     @Query() query: GetWeatherByLocationDto,
   ): Promise<Weather> {
