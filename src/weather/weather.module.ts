268d53de9aa90f8247d64ce8a444128ba88651cb
Bartłomiej B <bartlomiejb@MacBook-Pro-Bartomiej.local>
2024-01-07 15:24:27 +0100

    Remove HttpModule

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,11 +1,9 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 
 @Module({
-  imports: [HttpModule],
   providers: [WeatherService],
   controllers: [WeatherController],
 })
