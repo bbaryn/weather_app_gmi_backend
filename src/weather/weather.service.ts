7e3b48edad9762333ea5510057dd02c649ca0eb9
Bartłomiej B <bartlomiejb@MacBook-Pro-Bartomiej.local>
2024-01-11 13:24:45 +0100

    Add new endpoint

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -6,18 +6,15 @@ import { Location } from 'src/types/Location';
 import { Weather } from 'src/types/Weather';
 import { errorHandler } from 'src/utils/errorHandler';
 
+import { GetLocationListDto } from './dto/getLocationList.dto';
 import { GetWeatherByLocationDto } from './dto/getWeatherByLocation.dto';
 
 @Injectable()
 export class WeatherService {
   async getWeatherByLocation(query: GetWeatherByLocationDto): Promise<Weather> {
     try {
-      const location = await axios.get<Location[]>(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${query.location}&limit=5&appid=${process.env.OPENWEATHERMAP_API_KEY}`,
-      );
-
       const currentWeather = await axios.get<Weather>(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${location.data[0].lat}&lon=${location.data[0].lon}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`,
+        `https://api.openweathermap.org/data/2.5/weather?lat=${query.lat}&lon=${query.lon}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`,
       );
 
       return {
@@ -28,6 +25,30 @@ export class WeatherService {
         wind: currentWeather.data.wind,
         clouds: currentWeather.data.clouds,
       };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        errorHandler(error?.response.status);
+      }
+
+      return error.response;
+    }
+  }
+
+  async getLocationList(query: GetLocationListDto): Promise<Location[]> {
+    try {
+      const locations = await axios.get<Location[]>(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${query.location}&limit=5&appid=${process.env.OPENWEATHERMAP_API_KEY}`,
+      );
+
+      return locations.data.map((location) => {
+        return {
+          name: location.name,
+          lat: location.lat,
+          lon: location.lon,
+          country: location.country,
+          state: location.state,
+        };
+      });
     } catch (error) {
       if (axios.isAxiosError(error)) {
         errorHandler(error?.response.status);
